017f3af13f56e96c97c6bf4c792698bc53cfe357
Abdul Rafay <rafayabdul508@gmail.com>
2024-11-13 04:43:56 +0500

    initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <p className="text-3xl">hello world</p>
+      <Button>Click me</Button>
+    </>
+  );
+}
+
+export default App;
